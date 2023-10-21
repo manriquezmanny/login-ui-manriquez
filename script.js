0e72f3d90b1d87a28e821190dac269a4322ec78d
Manuel Manriquez <manriquezmanny@gmail.com>
2023-10-21 00:05:43 -0700

    Improved JavaScript toggle function design

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
+// Getting the SignIn and Register divs and storing them in variables. 
 var signInDiv = document.getElementById("sign-in-box");
 var registerDiv = document.getElementById("register-box");
 
-function register(){
-    signInDiv.style.display = "none";
-    registerDiv.style.display="flex";
-}
-
-function signIn(){
-    signInDiv.style.display = "flex";
-    registerDiv.style.display="none";
+// Created a function to toggle between Login and Register options on client side using DOM maipulation.
+function toggle(){
+    if (registerDiv.style.display === "none"){
+        signInDiv.style.display = "none";
+        registerDiv.style.display = "flex";
+    } else{
+        signInDiv.style.display = "flex";
+        registerDiv.style.display = "none"; 
+    }
 }
